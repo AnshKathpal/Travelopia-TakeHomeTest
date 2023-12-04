c950241b36c839e6dccb43819f1cbd71c28201fd
AnshKathpal <storage.2.3107@gmail.com>
2023-12-05 02:11:23 +0530

    updating toggle functionality

--- script.js
+++ script.js
@@ -1,16 +1,52 @@
 let toggleButton = document.getElementById("toggleButton");
-toggleButton.addEventListener("click", () => {
+let textDescription = document.querySelector(".textDescription");
+let originalContent = textDescription.innerHTML.trim();
+let contentLength = 300;
+let isToggle = true;
 
-    let textDescription = document.querySelector(".textDescription");
-    let originalContent = textDescription.textContent.trim()
-
-  let contentLength = 300;
-  if (textDescription.textContent.length > contentLength) {
-    textDescription.textContent =
-      textDescription.textContent.substring(0, contentLength) + "....";
-      toggleButton.textContent = "Show More"
-  }else{
-    textDescription.textContent = originalContent;
-    toggleButton.textContent = "Show Less"
+const textSplit = () => {
+  if (window.innerWidth <= 768) {
+    if (textDescription.textContent.length > contentLength) {
+      textDescription.innerHTML =
+        textDescription.innerHTML.substring(0, contentLength) + "....";
+      toggleButton.textContent = "Show More";
+      console.log(true);
+    } else {
+      toggleButton.textContent = "Show Less";
+      console.log(false);
+    }
+  } else {
+    textDescription.innerHTML = originalContent;
+    toggleButton.textContent = "Show Less";
+    console.log(false);
   }
-});
+};
+textSplit();
+window.addEventListener("resize", textSplit);
+
+const toggleFunctionality = () => {
+  if (window.innerWidth <= 768) {
+    toggleButton.style.display = "block";
+    toggleButton.addEventListener("click", () => {
+      if (!isToggle) {
+        if (textDescription.innerHTML.length > contentLength) {
+          textDescription.innerHTML =
+            textDescription.innerHTML.substring(0, contentLength) + "....";
+          toggleButton.textContent = "Show More";
+          console.log(true);
+        }
+      } else {
+        textDescription.innerHTML = originalContent;
+        toggleButton.textContent = "Show Less";
+        console.log(false);
+      }
+
+      isToggle = !isToggle;
+    });
+  } else {
+    toggleButton.style.display = "none";
+  }
+};
+
+toggleFunctionality();
+window.addEventListener("resize", toggleFunctionality);
