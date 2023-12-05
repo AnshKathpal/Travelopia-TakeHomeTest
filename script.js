5d4402a3b8b93cb2bac6d9265a2067776c5543b1
AnshKathpal <storage.2.3107@gmail.com>
2023-12-05 05:34:15 +0530

    email toggle

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let toggleButton = document.getElementById("toggleButton");
 let textDescription = document.querySelector(".textDescription");
 let originalContent = textDescription.innerHTML.trim();
-let contentLength = 300;
+let contentLength = 350;
 let isToggle = true;
 
 const textSplit = () => {
@@ -50,3 +50,39 @@ const toggleFunctionality = () => {
 
 toggleFunctionality();
 window.addEventListener("resize", toggleFunctionality);
+
+
+
+//  Toggling the Email Form for smaller screen sizes
+let enquiryEmail = document.getElementById("enquiry");
+let enquiryForm = document.getElementById("enquiryForm");
+let toggleSwitch = document.querySelector(".toggleSwitch")
+
+
+function adjustDisplay() {
+  if (window.innerWidth >= 768) {
+    enquiryForm.style.display = "block";
+    toggleSwitch.textContent = "-";
+  } else {
+    enquiryForm.style.display = "none";
+    toggleSwitch.textContent = "+";
+  }
+}
+adjustDisplay();
+enquiryEmail.addEventListener("click", () => {
+  if (window.innerWidth <= 768) {
+    if (enquiryForm.style.display == "none") {
+      enquiryForm.style.display = "block";
+      toggleSwitch.textContent = "-";
+    } else {
+      enquiryForm.style.display = "none";
+      toggleSwitch.textContent = "+";
+    }
+  }
+});
+
+window.addEventListener("resize", adjustDisplay);
+//  Toggling the Email Form for smaller screen sizes
+
+
+
